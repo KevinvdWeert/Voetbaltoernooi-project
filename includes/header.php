ece07d6164cdc04a889d7544821f375ac0be8f7a
KevinvdWeert <Ke.vandeweert@gmail.com>
2025-11-25 16:49:28 +0100

    Add JavaScript dependencies for Bootstrap and custom scripts in header

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,10 @@
     <title>Voetbaltoernooi Project</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 <body class="navbar-fixed">
     <header class="site-header">
